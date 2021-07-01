4c2e7ceaea2a5fa5fec76984073fe5c162657859
coderman <barslann58@gmail.com>
2021-07-01 14:03:40 -0700

    sent messages with key

---
+++ src/main/java/com/coderman/kafka/tutorial1/ProducerWithKeys.java
@@ -0,0 +1,65 @@
+package com.coderman.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerWithKeys {
+    public static Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallBack.class);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //Create producer properties
+        Properties properties = new Properties();
+//        properties.setProperty("bootstrap.servers","127.0.0.1:9092");  -> Old way. user producerconfig class
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            // create a producer record
+
+            String topic = "my-topic";
+            String value = "hello " + i;
+            String key = "id_" + i;
+            ProducerRecord<String,String> producerRecord =
+                    new ProducerRecord<>(topic,key,value);
+
+            logger.info("Key:" + key);
+            // send the data - asynchronous
+            producer.send(producerRecord, new Callback() {
+                // executes every time I get record being sent or there is an exception
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //executes every time a record is successfully sent or an exception is thrown
+                    if(e == null){
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n"+
+                                "Partition: " + recordMetadata.partition() +"\n"+
+                                "Offset: " + recordMetadata.offset() + "\n"+
+                                "Timestamp: " + recordMetadata.timestamp());
+                    }else{
+                        logger.error("Error while producing",e);
+
+                    }
+
+                }
+            }).get(); // block the .send() to make it synchronous
+        }
+
+
+
+        //flush data
+        producer.flush();
+
+        //flush and close producer
+        producer.close();
+
+    }
+}
